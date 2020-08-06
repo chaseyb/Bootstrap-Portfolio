5486d806daeefd2287bc7868d57e262b987f6685
Chase Johnson <Chase.b.johnson@gmail.com>
2020-08-05 20:24:53 -0700

    Fixed issue with contact button.

--- contactform.php
+++ contactform.php
@@ -6,7 +6,7 @@ if(isset($_POST['submit'])) {
     $mailFrom = $_POST["mail"];
     $message = $_POST['message'];
 
-    $mailTo = "connect@chasejohnson.dev";
+    $mailTo = "chasejohnson@gmail.com";
     $headers = "From: ".$mailFrom;
     $txt = "You have received an e-mail from ".$name.".\n\n".$message;
     
